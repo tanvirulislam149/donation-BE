6811bfcd58ce29a9d74c6d7991ef86641954e6cb
tanvir <tanvirulislam149@gmail.com>
2024-02-07 22:30:24 +0600

    getdon by id

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get("/getDonation/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationCollection.findOne(query);
+      res.send(result);
+    })
+
   } finally {
 
   }

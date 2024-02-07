fdfbee9ddd22019005262296f91c04c3175e9f7c
tanvir <tanvirulislam149@gmail.com>
2024-02-07 17:18:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1gt7l1o.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+
+    app.get('/asdf', (req, res) => {
+      res.send('Hello World from donation db')
+    })
+
+    const donationCollection = client.db("donation").collection("allDonation");
+
+    app.get("/getAllDonation", async (req, res) => {
+      const query = {};
+      const cursor = donationCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+  } finally {
+
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World from donation')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
